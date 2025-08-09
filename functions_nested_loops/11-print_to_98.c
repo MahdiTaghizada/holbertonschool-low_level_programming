ebd3f31a13aa4f67ebe9b28fc563a5b071bb5c1f
MahdiTaghizada <tagizademehti@gmail.com>
2025-08-09 11:49:00 +0000

    l

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,8 +2,8 @@
  * File: 11-print_to_98.c
  * Auth: Brennan D Baraban
  */
-
-#include <main.h>
+#include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - Prints all natural numbers from input to 98,
