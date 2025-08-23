a77b926ea432ff7a8b080d280465f18befa1510a
MahdiTaghizade <tagizademehti@gmail.com>
2025-08-23 18:16:47 +0400

    1

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+    if(argc <= 2) {
+        printf("Error\n");
+        return 1;
+    }
+    else {
+
+        int a = atoi(argv[1]);
+        int b = atoi(argv[2]);
+        printf("%d\n", a * b);
+    }
+}
