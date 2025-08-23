dcd0f3be8b42b92e3e14e92b5505b1273cd99771
MahdiTaghizade <tagizademehti@gmail.com>
2025-08-23 20:08:35 +0400

    f

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -8,20 +8,21 @@
  *
  * Return: pointer to the allocated memory
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+void	*_calloc(unsigned int nmemb, unsigned int size)
 {
-    unsigned long total, i;
-    void *ptr;
+	unsigned long	total, i;
+	void		*ptr;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-    total = nmemb * size;
-    ptr = malloc(total);
-    if (ptr == NULL)
-        return (NULL);
-    for (i = 0; i < total; i++)
-        ((char *)ptr)[i] = 0;
+	total = nmemb * size;
+	ptr = malloc(total);
+	if (ptr == NULL)
+		return (NULL);
 
-    return (ptr);
+	for (i = 0; i < total; i++)
+		((char *)ptr)[i] = 0;
+
+	return (ptr);
 }
