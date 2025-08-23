9542f7e68a08af302048da1828d3da77c11d9c9f
MahdiTaghizade <tagizademehti@gmail.com>
2025-08-23 20:06:04 +0400

    f

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,7 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 
-
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: count of bytes
+ * @size: size of each individual element
+ *
+ * Return: pointer to the allocated memory
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned long total, i;
