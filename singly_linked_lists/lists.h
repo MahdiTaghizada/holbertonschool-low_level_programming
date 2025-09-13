c777550241040136d110d5b300a300eb9a757636
MahdiTaghizade <tagizademehti@gmail.com>
2025-09-13 11:04:37 +0400

    n

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+#include <stddef.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+size_t print_list.c(const list_t *h);
+
+#endif
