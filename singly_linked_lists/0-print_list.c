4008b14b1d0ac8e48584a30d457e596da1061594
MahdiTaghizade <tagizademehti@gmail.com>
2025-09-13 11:19:54 +0400

    a

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,14 +8,14 @@
  *
  * Return: size of list
  */
-size_t print_list.c(const list_t *h)
+size_t print_list(const list_t *h)
 {
     int i;
     for(i=0; h!= NULL ; i++)
     {
         printf("[%d] %s\n", 
-            h->str != NULL? 0: h->len,
-            h->str != NULL? "(nil)": h->str);
+            h->str == NULL? 0: h->len,
+            h->str == NULL? "(nil)": h->str);
         h=h->next;
     }
     return(i);
