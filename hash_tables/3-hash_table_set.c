0ef9e5333f31040e160085b615f0ca1563c559bf
MahdiTaghizade <tagizademehti@gmail.com>
2025-09-20 16:58:39 +0400

    a

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -4,9 +4,12 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value associated with the key
  *
- * Return: Always EXIT_SUCCESS.
+ * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
